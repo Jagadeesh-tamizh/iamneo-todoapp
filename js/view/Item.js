d98ccc20cda8185b706e7b3cc81730d05a4bd175
Jagadeesh <84759769+Jagadeesh-tamizh@users.noreply.github.com>
2022-10-18 14:17:49 +0530

    Update Item.js

--- js/view/Item.js
+++ js/view/Item.js
@@ -33,11 +33,6 @@ export default class Item {
     };
 
     this.elements.input.addEventListener("blur", onBlur);
-    // this.elements.root.addEventListener("dblclick", () => {
-    //   KanbanAPI.deleteItem(id);
-    //   this.elements.input.removeEventListener("blur", onBlur);
-    //   this.elements.root.parentElement.removeChild(this.elements.root);
-    // });
 
     this.elements.input.button.addEventListener("click", () => {
       KanbanAPI.deleteItem(id);
